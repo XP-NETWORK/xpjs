c4be42a927c8ae070c30d855526632fe88d3e96e
rupansh <dima@xp.network>
2021-07-28 23:35:01 +0530

    polkadot: mint: convert to hex before sending

--- src/helpers/polkadot.ts
+++ src/helpers/polkadot.ts
@@ -43,6 +43,21 @@ export type PolkadotPalletHelper = PolkadotHelper &
   MintNft<Signer, Uint8Array, void> &
   ListNft<EasyAddr, string, string>;
 
+
+const LUT_HEX_4b = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+const LUT_HEX_8b = new Array(0x100);
+for (let n = 0; n < 0x100; n++) {
+  LUT_HEX_8b[n] = `${LUT_HEX_4b[(n >>> 4) & 0xF]}${LUT_HEX_4b[n & 0xF]}`;
+}
+// End Pre-Init
+export function toHex(buffer: Uint8Array) {
+  let out = '';
+  for (let idx = 0, edx = buffer.length; idx < edx; idx++) {
+    out += LUT_HEX_8b[buffer[idx]];
+  }
+  return out;
+}
+
 async function basePolkadotHelper(
   node_uri: string
   ): Promise<[BasePolkadot, ApiPromise]> {
@@ -158,7 +173,7 @@ export const polkadotPalletHelperFactory: (
         }
 
         await api.tx.sudo.sudo(
-          api.tx.nft.mint(addr, info)
+          api.tx.nft.mint(addr, toHex(info))
         ).signAndSend(sudoSigner);
     },
     async listNft(
