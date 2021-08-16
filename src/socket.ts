f599351007c469879dcb7ee8b665d5c27a0a360c
rupansh <dima@xp.network>
2021-08-16 18:53:35 +0530

    socket: preinit for chains

--- src/socket.ts
+++ src/socket.ts
@@ -4,7 +4,7 @@ type ChainAwaiter = {
 	[action_id: string]: {
 		resolve?: (hash: string) => void;
 		event_res?: string;
-	}
+	} | undefined;
 };
 
 type TxResBuf = {
@@ -17,12 +17,18 @@ export type TxnSocketHelper = {
 
 export function txnSocketHelper(uri: string, options?: Partial<SocketOptions & ManagerOptions>): TxnSocketHelper {
     const socket = io(uri, options);
-	const buf: TxResBuf = {};
+	const buf: TxResBuf = {
+		1: {},
+		2: {},
+		3: {},
+		4: {},
+		5: {}
+	};
 
     function add_event(chain: number, id: string, hash: string) {
-		const resolve = buf[chain][id].resolve;
+		const resolve = buf[chain][id]?.resolve;
 		if (resolve === undefined) {
-			buf[chain][id].event_res = hash;
+			buf[chain][id] = { event_res: hash };
 			return;
 		}
 		resolve(hash);
@@ -44,14 +50,14 @@ export function txnSocketHelper(uri: string, options?: Partial<SocketOptions & M
 
     return {
 		async waitTxHash(chain: number, action_id: string): Promise<string> {
-			const hash = buf[chain][action_id].event_res;
+			const hash = buf[chain][action_id]?.event_res;
 			if (hash !== undefined) {
-				buf[chain][action_id].event_res = undefined;
+				buf[chain][action_id] = undefined;
 				return hash;
 			}
 
 			const hashP: Promise<string> = new Promise(r => {
-				buf[chain][action_id].resolve = r;
+				buf[chain][action_id] = { resolve: r };
 			});
 
 			return await hashP;
