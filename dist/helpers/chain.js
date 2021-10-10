634a8a15bfcb23de5416b7f6a2808e76063a90cf
imsk17 <imsumitkumar17@gmail.com>
2021-10-10 17:51:14 +0530

    releases: dist/ is redundant now Hence the cleanup

--- dist/helpers/chain.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConcurrentSendError = void 0;
-function ConcurrentSendError() {
-    return new Error("concurrent_send");
-}
-exports.ConcurrentSendError = ConcurrentSendError;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2hhaW4uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaGVscGVycy9jaGFpbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUE0S0EsU0FBZ0IsbUJBQW1CO0lBQ2pDLE9BQU8sSUFBSSxLQUFLLENBQUMsaUJBQWlCLENBQUMsQ0FBQztBQUN0QyxDQUFDO0FBRkQsa0RBRUMifQ==
