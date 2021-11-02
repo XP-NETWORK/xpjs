8811d9b41f31b60f92599b6686652455077b2251
imsk17 <imsumitkumar17@gmail.com>
2021-11-02 13:55:18 +0000

    Build: (2fa7117) fix(mainnet-uri): update the mainnet URIs

--- dist/consts.d.ts
+++ dist/consts.d.ts
@@ -8,7 +8,7 @@ export declare enum TestNetRpcUri {
     ELROND = "https://devnet-api.elrond.com",
     HECO = "https://http-testnet.hecochain.com",
     BSC = "https://data-seed-prebsc-1-s1.binance.org:8545",
-    ROPSTEN = "https://ropsten.infura.io/v3/182b3d3fb2d14d5fbe7421348624d1ce",
+    ROPSTEN = "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
     AVALANCHE = "https://api.avax-test.network/ext/bc/C/rpc",
     POLYGON = "https://matic-testnet-archive-rpc.bwarelabs.com",
     FANTOM = "https://rpc.testnet.fantom.network/",
@@ -17,16 +17,16 @@ export declare enum TestNetRpcUri {
     HARMONY = "https://api.s0.b.hmny.io"
 }
 export declare enum MainNetRpcUri {
-    ELROND = "https://devnet-api.elrond.com",
-    HECO = "https://http-testnet.hecochain.com",
-    BSC = "https://data-seed-prebsc-1-s1.binance.org:8545",
-    ETHEREUM = "https://ropsten.infura.io/v3/182b3d3fb2d14d5fbe7421348624d1ce",
-    AVALANCHE = "https://api.avax-test.network/ext/bc/C/rpc",
-    POLYGON = "https://matic-testnet-archive-rpc.bwarelabs.com",
-    FANTOM = "https://rpc.testnet.fantom.network/",
-    TRON = "https://api.shasta.trongrid.io/",
-    CELO = "https://alfajores-forno.celo-testnet.org",
-    HARMONY = "https://api.s0.b.hmny.io"
+    ELROND = "https://gateway.elrond.com",
+    HECO = "https://http-mainnet-node.huobichain.com",
+    BSC = "https://bsc-dataseed.binance.org/",
+    ETHEREUM = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+    AVALANCHE = "https://api.avax.network/ext/bc/C/rpc",
+    POLYGON = "https://rpc-mainnet.matic.network",
+    FANTOM = "https://rpc.ftm.tools/",
+    TRON = "https://api.trongrid.io/",
+    CELO = "https://forno.celo.org",
+    HARMONY = "https://api.harmony.one"
 }
 export declare type ChainNonce<T, P> = number & ThisType<T> & ThisType<P>;
 export declare type ElrondNonce = ChainNonce<ElrondHelper, ElrondParams>;
