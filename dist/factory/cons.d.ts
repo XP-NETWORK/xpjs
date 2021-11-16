1e787988d1b5466347998034e554ad462f2b4a5f
rupansh <rupanshsekar@hotmail.com>
2021-11-16 09:53:25 +0000

    Build: (065fc2b) nft-list: update for tron

--- dist/factory/cons.d.ts
+++ dist/factory/cons.d.ts
@@ -3,5 +3,5 @@ import { ElrdNftListRepo, EthNftJson, MoralisNftListRepo, NftListRepo } from "xp
 import TronWeb from "tronweb";
 export declare function elrondNftList(proxy: string): ElrdNftListRepo;
 export declare function moralisNftList(server: string, appId: string): MoralisNftListRepo;
-export declare function tronListNft(tronWeb: TronWeb, tronScan: string): NftListRepo<string, EthNftJson>;
+export declare function tronListNft(tronWeb: TronWeb, tronScan: string, xpnftAddr: string): NftListRepo<string, EthNftJson>;
 export declare function exchangeRateRepo(baseUrl: string): ExchangeRateRepo;
