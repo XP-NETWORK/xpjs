4f92f2011e430e2cd6c49d124d7f5eb2dddbd053
imsk17 <imsumitkumar17@gmail.com>
2021-12-07 16:59:47 +0000

    Build: (4f597a7) feat(factory): use nft lister for algorand

--- dist/factory/cons.d.ts
+++ dist/factory/cons.d.ts
@@ -1,2 +1,3 @@
 import { ExchangeRateRepo } from "crypto-exchange-rate";
 export declare function exchangeRateRepo(baseUrl: string): ExchangeRateRepo;
+export declare const algoListNft: (baseUri: string) => import("xpnet-nft-list").NftListRepo<string, import("xpnet-nft-list").AlgoNft>;
