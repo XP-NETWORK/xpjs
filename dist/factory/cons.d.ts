84b628d6bcb402cb3b6627692ec645282fec7ed8
rupansh <rupanshsekar@hotmail.com>
2021-12-08 12:02:23 +0000

    Build: (0ada262) cleanup nft-list dep

--- dist/factory/cons.d.ts
+++ dist/factory/cons.d.ts
@@ -1,3 +1,2 @@
 import { ExchangeRateRepo } from "crypto-exchange-rate";
 export declare function exchangeRateRepo(baseUrl: string): ExchangeRateRepo;
-export declare const algoListNft: (baseUri: string) => import("xpnet-nft-list").NftListRepo<string, import("xpnet-nft-list").AlgoNft>;
