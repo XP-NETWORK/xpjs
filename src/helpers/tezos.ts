1de5c016d9d28684bbd0a19049a14cef468e027c
imsk17 <imsumitkumar17@gmail.com>
2021-12-22 20:31:19 +0530

    feat(tezos): fix some things

--- src/helpers/tezos.ts
+++ src/helpers/tezos.ts
@@ -1,11 +1,13 @@
 import {
   BalanceCheck,
+  ChainNonceGet,
   EstimateTxFees,
   MintNft,
   NftMintArgs,
   TransferNftForeign,
   UnfreezeForeignNft,
   ValidateAddress,
+  WrappedNftCheck,
 } from "..";
 import { Signer, TezosToolkit, TransactionOperation } from "@taquito/taquito";
 import { validatePublicKey } from "@taquito/utils";
@@ -38,12 +40,15 @@ export type TezosHelper = TransferNftForeign<
     TransactionOperation
   > &
   ValidateAddress &
-  EstimateTxFees<BigNumber>;
+  EstimateTxFees<BigNumber> &
+  ChainNonceGet &
+  WrappedNftCheck<TezosNftInfo>;
 
 export type TezosParams = {
   Tezos: TezosToolkit;
   xpnftAddress: string;
   bridgeAddress: string;
+  validators: string[];
 };
 
 export async function tezosHelperFactory({
@@ -93,6 +98,12 @@ export async function tezosHelperFactory({
     },
     async validateAddress(adr) {
       return Promise.resolve(validatePublicKey(adr) === 3);
+    },
+    isWrappedNft(nft) {
+      return nft.native.contract.toLowerCase() === xpnftAddress.toLowerCase();
+    },
+    getNonce() {
+      return 0x11;
     },
     async estimateValidateTransferNft(_to, _uri) {
       return Promise.resolve(NFT_TRANSFER_COST);
