ace92cd8efd436718a23c86d0429897688968849
rupansh <rupanshsekar@hotmail.com>
2022-01-17 15:42:36 +0000

    Build: (21f2a40) tezos fixes and improvements

--- dist/helpers/tezos.d.ts
+++ dist/helpers/tezos.d.ts
@@ -1,4 +1,4 @@
-import { BalanceCheck, ChainNonceGet, EstimateTxFees, MintNft, NftMintArgs, TransferNftForeign, UnfreezeForeignNft, ValidateAddress, WrappedNftCheck } from "..";
+import { BalanceCheck, ChainNonceGet, EstimateTxFees, MintNft, NftInfo, NftMintArgs, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress, WrappedNftCheck } from "..";
 import { Signer, TezosToolkit, TransactionOperation } from "@taquito/taquito";
 import BigNumber from "bignumber.js";
 declare type TezosSigner = Signer;
@@ -6,7 +6,9 @@ declare type TezosNftInfo = {
     contract: string;
     id: string;
 };
-export declare type TezosHelper = TransferNftForeign<TezosSigner, string, BigNumber, TezosNftInfo, TransactionOperation> & MintNft<TezosSigner, NftMintArgs, TransactionOperation> & BalanceCheck<string, BigNumber> & UnfreezeForeignNft<TezosSigner, string, BigNumber, TezosNftInfo, TransactionOperation> & ValidateAddress & EstimateTxFees<BigNumber, TezosNftInfo> & ChainNonceGet & WrappedNftCheck<TezosNftInfo>;
+export declare type TezosHelper = TransferNftForeign<TezosSigner, string, BigNumber, TezosNftInfo, TransactionOperation> & MintNft<TezosSigner, NftMintArgs, TransactionOperation> & BalanceCheck<string, BigNumber> & UnfreezeForeignNft<TezosSigner, string, BigNumber, TezosNftInfo, TransactionOperation> & ValidateAddress & EstimateTxFees<BigNumber, TezosNftInfo> & ChainNonceGet & WrappedNftCheck<TezosNftInfo> & Pick<PreTransfer<Signer, TezosNftInfo, string>, "preTransfer"> & {
+    isApproved(signer: Signer, nft: NftInfo<TezosNftInfo>): Promise<boolean>;
+};
 export declare type TezosParams = {
     Tezos: TezosToolkit;
     xpnftAddress: string;
