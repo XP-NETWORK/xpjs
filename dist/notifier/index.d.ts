b345a631207a77b786d67c84992ed957a905309d
AlexeyAdoniev <AlexeyAdoniev@users.noreply.github.com>
2022-03-02 17:20:40 +0000

    Build: (5d7868a) factories addresses change

--- dist/notifier/index.d.ts
+++ dist/notifier/index.d.ts
@@ -2,6 +2,6 @@ export declare type EvNotifier = ReturnType<typeof evNotifier>;
 export declare function evNotifier(url: string): {
     notifyWeb3(chainNonce: number, txHash: string): Promise<void>;
     notifyTron(txHash: string): Promise<void>;
-    notifyElrond(txHash: string): Promise<void>;
+    notifyElrond(txHash: string, sender: string, uris: string[]): Promise<void>;
     notifyTezos(txHash: string): Promise<void>;
 };
