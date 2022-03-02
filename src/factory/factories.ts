8e62b5799f01c07670eeb107224c639af32721a9
Yura Dukhno <91468889+YuraDXPNetwork@users.noreply.github.com>
2022-03-02 09:44:49 +0200

    Update factories.ts

--- src/factory/factories.ts
+++ src/factory/factories.ts
@@ -223,9 +223,9 @@ export namespace ChainFactoryConfigs {
         provider: new ethers.providers.JsonRpcProvider(MainNetRpcUri.FANTOM),
         erc721Minter: "0xC81D46c6F2D59182c5A64FD5C372266c98985AdF",
         erc1155Minter: "0x146a99Ff19ece88EC87f5be03085cA6CD3163E15",
-        erc1155_addr: "0x9b2bACF4E69c81EF4EF42da84872aAC39ce7EC62",
-        erc721_addr: "0x7E8493F59274651Cc0919feCf12E6A77153cdA72",
-        minter_addr: "0x73E8deFC951D228828da35Ff8152f25c1e5226fa",
+        erc1155_addr: "0x93239b1CF8CAd847f387735876EdBa7D75ae4f7A",
+        erc721_addr: "0xa66dA346C08dD77bfB7EE5E68C45010B6F2538ff",
+        minter_addr: "0xF5e0c79CB0B7e7CF6Ad2F9779B01fe74F958964a",
         nonce: Chain.FANTOM,
       },
       bscParams: {
