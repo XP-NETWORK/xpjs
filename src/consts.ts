1fb631f3110fa707b62a9ed348bc3a70e7347f74
imsk17 <imsumitkumar17@gmail.com>
2022-03-03 12:50:43 +0530

    configs: use the seprate namespace for configs

--- src/consts.ts
+++ src/consts.ts
@@ -11,7 +11,6 @@ import {
   AlgorandHelper,
   algorandHelper,
 } from "./helpers/algorand";
-import { AppConfig } from "./factory";
 import { TezosHelper, tezosHelperFactory, TezosParams } from "./helpers/tezos";
 import { ChainNonce, InferChainH, InferChainParam } from "./type-utils";
 
@@ -295,26 +294,4 @@ CHAIN_INFO.set(Chain.IOTEX, {
   chainId: 4689,
 });
 
-export const Config: AppConfig = {
-  exchangeRateUri: "https://testing-bridge.xp.network/exchange/",
-  nftListUri: "https://indexnft.herokuapp.com",
-  nftListAuthToken:
-    "eyJhbGciOiJFUzI1NiJ9.eyJhdXRob3JpdHkiOjEsImlhdCI6MTYzODk2MjMzOCwiZXhwIjoxNjQ2NzM4MzM4fQ.9eQMNMtt-P6myPlji7nBC9PAwTftd0qQvwnIZSt4ycM4E45NpzCF0URsdYj_YN_xqQKQpcHiZu1o4EXjJa_-Zw",
-  txSocketUri: "transaction-socket.xp.network",
-  tronScanUri: "https://apilist.tronscan.org/api/",
-  heartbeatUri: "https://xpheartbeat.herokuapp.com",
-  wrappedNftPrefix: "https://nft.xp.network/w/",
-};
-
-<<<<<<< Updated upstream
 export const FEE_MARGIN = { min: 0.5, max: 5 };
-=======
-export const TestnetConfig: AppConfig = {
-  exchangeRateUri: "https://testing-bridge.xp.network/exchange/",
-  nftListUri:"https://testing-nft-index.herokuapp.com",
-  nftListAuthToken:"eyJhbGciOiJFUzI1NiJ9.eyJhdXRob3JpdHkiOjEsImlhdCI6MTY0NDIzMDE0NiwiZXhwIjoxNjUyMDA2MTQ2fQ.gX6Auj4hFLdpem5Pk2kAnH71I19iGXfBYjmQrXylMFX_R1yzDEbylVAOLW6kABH9VjnLbAnCRdQvLaQIFmMUpw",
-  txSocketUri:"https://testnet-tx-socket.herokuapp.com",
-  tronScanUri:"https://apilist.tronscan.org/api/",
-  heartbeatUri:"https://testnet-validator-pinger.herokuapp.com/"
-}
->>>>>>> Stashed changes
