bb1d982580ce6f19c809e824c7e9dbcb33cd25f3
imsk17 <imsumitkumar17@gmail.com>
2022-06-02 12:30:42 +0000

    Build: (37d5514) secret: more fixes

---
+++ dist/helpers/secret.d.ts
@@ -0,0 +1,26 @@
+import { SecretNetworkClient } from "secretjs";
+import { EvNotifier } from "../notifier";
+import { BalanceCheck, ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, GetProvider, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
+export declare type SecretNftInfo = {
+    contract: string;
+    contractHash: string;
+    token_id: string;
+};
+declare type SecretSigner = SecretNetworkClient;
+export declare type SecretHelper = TransferNftForeign<SecretSigner, SecretNftInfo, string> & UnfreezeForeignNft<SecretSigner, SecretNftInfo, string> & ValidateAddress & EstimateTxFees<SecretNftInfo> & ChainNonceGet & PreTransfer<SecretSigner, SecretNftInfo, string> & BalanceCheck & GetFeeMargins & {
+    XpNft: string;
+} & GetProvider<SecretNetworkClient>;
+export declare type SecretContract = {
+    contractAddress: string;
+    codeHash: string;
+};
+export declare type SecretParams = {
+    rpcUrl: string;
+    chainId: string;
+    notifier: EvNotifier;
+    bridge: SecretContract;
+    xpnft: SecretContract;
+    feeMargin: FeeMargins;
+};
+export declare function secretHelperFactory(p: SecretParams): Promise<SecretHelper>;
+export {};
