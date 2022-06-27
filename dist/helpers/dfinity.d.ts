ffc8eea9bd49056e3fa7dcc05e8f8333c09f2ac5
rupansh <rupanshsekar@hotmail.com>
2022-06-27 14:32:42 +0000

    Build: (da7979b) dfinity: add balance check

--- dist/helpers/dfinity.d.ts
+++ dist/helpers/dfinity.d.ts
@@ -1,7 +1,7 @@
 import { HttpAgent, Identity } from "@dfinity/agent";
 import { Principal } from "@dfinity/principal";
 import { EvNotifier } from "../notifier";
-import { ChainNonceGet, EstimateTxFees, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
+import { BalanceCheck, ChainNonceGet, EstimateTxFees, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
 export declare type DfinitySigner = Identity;
 export declare type DfinityNft = {
     canisterId: string;
@@ -9,7 +9,7 @@ export declare type DfinityNft = {
 };
 export declare type DfinityHelper = ChainNonceGet & TransferNftForeign<DfinitySigner, DfinityNft, string> & UnfreezeForeignNft<DfinitySigner, DfinityNft, string> & EstimateTxFees<DfinityNft> & ValidateAddress & {
     XpNft: string;
-} & Pick<PreTransfer<DfinitySigner, DfinityNft, string>, "preTransfer">;
+} & Pick<PreTransfer<DfinitySigner, DfinityNft, string>, "preTransfer"> & BalanceCheck;
 export declare type DfinityParams = {
     agent: HttpAgent;
     bridgeContract: Principal;
