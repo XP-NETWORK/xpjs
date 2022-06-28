16fa6651e9602091df9dd9238bc4ff1c8cf35436
rupansh <rupanshsekar@hotmail.com>
2022-06-28 16:54:20 +0000

    Build: (4a5de22) tezos: fix isApprovedForMinter

--- dist/helpers/tezos.d.ts
+++ dist/helpers/tezos.d.ts
@@ -8,7 +8,7 @@ export declare type TezosNftInfo = {
     token_id: string;
 };
 export declare type TezosHelper = TransferNftForeign<TezosSigner, TezosNftInfo, string> & MintNft<TezosSigner, NftMintArgs, string> & BalanceCheck & UnfreezeForeignNft<TezosSigner, TezosNftInfo, string> & ValidateAddress & EstimateTxFees<TezosNftInfo> & ChainNonceGet & Pick<PreTransfer<Signer, TezosNftInfo, string>, "preTransfer"> & {
-    isApprovedForMinter(signer: Signer, nft: NftInfo<TezosNftInfo>): Promise<boolean>;
+    isApprovedForMinter(signer: TezosSigner, nft: NftInfo<TezosNftInfo>): Promise<boolean>;
 } & {
     approveForMinter(address: NftInfo<TezosNftInfo>, sender: TezosSigner): Promise<string | undefined>;
 } & {
