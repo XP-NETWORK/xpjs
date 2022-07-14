4f98e591450050ab998bca1940254cee7cfd5b3b
imsk17 <imsumitkumar17@gmail.com>
2022-07-14 15:07:41 +0530

    factory: chain-info: update decimals for godwoken

--- src/consts.ts
+++ src/consts.ts
@@ -336,7 +336,7 @@ CHAIN_INFO.set(Chain.GODWOKEN, {
   blockExplorerUrl: "https://aggron.layerview.io/tx/",
   constructor: web3HelperFactory,
   nonce: 0x16,
-  decimals: 1e8,
+  decimals: 1e18,
   currency: SupportedCurrency.CKB,
   chainId: 868455272153094,
 });
