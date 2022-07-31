8610d0e808f23140d2a432a31b6749318d746996
AlexeyAdoniev <Alexeya@xp.network>
2022-07-31 16:39:09 +0300

    quickfix

--- src/helpers/web3.ts
+++ src/helpers/web3.ts
@@ -283,17 +283,17 @@ export async function web3HelperFactory(
             .get(
               "https://gpoly.blockscan.com/gasapi.ashx?apikey=key&method=pendingpooltxgweidata"
             )
-            .catch(async (e) => {
+            .catch(async () => {
               return await axios.get(
                 "https://gasstation-mainnet.matic.network/v2"
               );
             });
-          const { result } = res.data;
-          const fast = result["rapidgaspricegwei"] || res.data.fast;
+          const { result, fast } = res.data;
+          const trackerGas = result?.rapidgaspricegwei || fast?.maxFee;
 
-          if (fast) {
+          if (trackerGas) {
             const sixtyGwei = ethers.utils.parseUnits(
-              Math.ceil(fast.maxFee || fast).toString(),
+              Math.ceil(trackerGas).toString(),
               "gwei"
             );
             utx.maxFeePerGas = sixtyGwei;
