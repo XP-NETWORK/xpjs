5613e06b636fda6b6f1c04085d1fee6b04d5bcbb
imsk17 <imsumitkumar17@gmail.com>
2022-08-04 15:01:11 +0000

    Build: (7594aaf) factory: helper: fix dfinity

--- dist/helpers/dfinity.d.ts
+++ dist/helpers/dfinity.d.ts
@@ -1,21 +1,36 @@
-import { HttpAgent, Identity } from "@dfinity/agent";
+import { HttpAgent, Identity, SubmitResponse } from "@dfinity/agent";
+import { AccountIdentifier } from "@dfinity/nns";
 import { Principal } from "@dfinity/principal";
 import { EvNotifier } from "../notifier";
-import { BalanceCheck, ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
+import { BalanceCheck, ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, MintNft, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
 export declare type DfinitySigner = Identity;
 export declare type DfinityNft = {
     canisterId: string;
     tokenId: string;
 };
+export declare type DfinityMintArgs = {
+    canisterId?: string;
+    uri: string;
+};
+export declare type User = {
+    principal: Principal;
+} | {
+    address: AccountIdentifier;
+};
+export interface MintRequest {
+    to: User;
+    metadata: [] | [Array<number>];
+}
 export declare type DfinityHelper = ChainNonceGet & TransferNftForeign<DfinitySigner, DfinityNft, string> & UnfreezeForeignNft<DfinitySigner, DfinityNft, string> & EstimateTxFees<DfinityNft> & ValidateAddress & {
     XpNft: string;
-} & Pick<PreTransfer<DfinitySigner, DfinityNft, string>, "preTransfer"> & BalanceCheck & GetFeeMargins;
+} & Pick<PreTransfer<DfinitySigner, DfinityNft, string>, "preTransfer"> & BalanceCheck & GetFeeMargins & MintNft<DfinitySigner, DfinityMintArgs, SubmitResponse>;
 export declare type DfinityParams = {
     agent: HttpAgent;
     bridgeContract: Principal;
     xpnftId: Principal;
     notifier: EvNotifier;
     feeMargin: FeeMargins;
+    umt: Principal;
 };
 export declare function dfinityHelper(args: DfinityParams): Promise<DfinityHelper>;
 //# sourceMappingURL=dfinity.d.ts.map
