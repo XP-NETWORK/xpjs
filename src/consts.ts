7382eadc5e061803452eb20431e76e08b77669bd
Dima Brook <dima@xp.network>
2022-08-14 13:12:12 +0300

    add Mainnet Secret Network

--- src/consts.ts
+++ src/consts.ts
@@ -73,6 +73,7 @@ export enum MainNetRpcUri {
   GODWOKEN = "https://v1.mainnet.godwoken.io/rpc",
   GATECHAIN = "https://evm.gatenode.cc",
   VECHAIN = "https://sync-mainnet.veblocks.net",
+  SECRET = "https://secret-4.api.trivium.network:9091",
   // TODO: Algorand
 }
 
