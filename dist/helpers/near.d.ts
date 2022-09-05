b04d8a01afd8dfcad811f60992bb34b4f85c5438
imsk17 <imsumitkumar17@gmail.com>
2022-09-05 12:51:58 +0000

    Build: (402b6d0) Skale: Change sFUEL to SKL

--- dist/helpers/near.d.ts
+++ dist/helpers/near.d.ts
@@ -1,15 +1,18 @@
 import { Account } from "near-api-js";
-import { ChainNonceGet, EstimateTxFees, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
+import { ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "./chain";
 export declare type NearParams = {
     networkId: string;
     nonce: number;
     rpcUrl: string;
     bridge: string;
     xpnft: string;
+    feeMargin: FeeMargins;
 };
 export declare type NearNFT = {
     tokenId: string;
 };
-export declare type NearHelper = ChainNonceGet & TransferNftForeign<Account, NearNFT, string> & UnfreezeForeignNft<Account, NearNFT, string> & EstimateTxFees<NearNFT> & ValidateAddress;
-export declare function nearHelperFactory({ networkId, bridge, rpcUrl, }: NearParams): Promise<NearHelper>;
+export declare type NearHelper = ChainNonceGet & TransferNftForeign<Account, NearNFT, string> & UnfreezeForeignNft<Account, NearNFT, string> & EstimateTxFees<NearNFT> & ValidateAddress & {
+    XpNft: string;
+} & GetFeeMargins;
+export declare function nearHelperFactory({ networkId, bridge, rpcUrl, xpnft, feeMargin, }: NearParams): Promise<NearHelper>;
 //# sourceMappingURL=near.d.ts.map
