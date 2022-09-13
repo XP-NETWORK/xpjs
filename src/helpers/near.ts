c3fc6dda33b8e177915d2ca689cd3be21dac3c9d
imsk17 <imsumitkr17@gmail.com>
2022-09-13 16:20:04 +0530

    near: migrate to underlying contract implementation

--- src/helpers/near.ts
+++ src/helpers/near.ts
@@ -1,13 +1,11 @@
 import BigNumber from "bignumber.js";
-import { BN } from "bn.js";
+import BN from "bn.js";
 
+import { Account, connect, DEFAULT_FUNCTION_CALL_GAS, Near } from "near-api-js";
 import {
-  Account,
-  connect,
-  Contract,
-  DEFAULT_FUNCTION_CALL_GAS,
-  Near,
-} from "near-api-js";
+  FinalExecutionOutcome,
+  getTransactionLastResult,
+} from "near-api-js/lib/providers";
 import { Chain } from "../consts";
 import {
   ChainNonceGet,
@@ -19,6 +17,8 @@ import {
   UnfreezeForeignNft,
   ValidateAddress,
 } from "./chain";
+
+type NearTxResult = [FinalExecutionOutcome, any];
 
 export type NearParams = {
   networkId: string;
@@ -33,14 +33,13 @@ export type NearNFT = {
 };
 
 export type NearHelper = ChainNonceGet &
-  TransferNftForeign<Account, NearNFT, string> &
-  UnfreezeForeignNft<Account, NearNFT, string> &
+  TransferNftForeign<Account, NearNFT, NearTxResult> &
+  UnfreezeForeignNft<Account, NearNFT, NearTxResult> &
   EstimateTxFees<NearNFT> &
   ValidateAddress & {
     XpNft: string;
   } & GetFeeMargins &
   GetProvider<Near>;
-
 export async function nearHelperFactory({
   networkId,
   bridge,
@@ -53,13 +52,6 @@ export async function nearHelperFactory({
     networkId,
     headers: {},
   });
-
-  const getMinter = async (connection: Account) => {
-    return new Contract(connection, bridge, {
-      changeMethods: ["freeze_nft", "withdraw_nft"],
-      viewMethods: [],
-    }) as any;
-  };
 
   return {
     async estimateValidateTransferNft(_to, _metadata, _mintWith) {
@@ -81,19 +73,20 @@ export async function nearHelperFactory({
       mint_with,
       gasLimit
     ) {
-      const minter = await getMinter(sender);
-      const resp = minter.freeze_nft(
-        {
+      const result = await sender.functionCall({
+        contractId: bridge,
+        args: {
           token_id: id.native.tokenId,
           chain_nonce,
           to,
           amt: new BigNumber(txFees),
           mint_with,
         },
-        gasLimit ?? DEFAULT_FUNCTION_CALL_GAS,
-        txFees
-      );
-      return resp;
+        methodName: "freeze_nft",
+        attachedDeposit: new BN(txFees.toString()),
+        gas: new BN(gasLimit?.toString() ?? DEFAULT_FUNCTION_CALL_GAS),
+      });
+      return [result, getTransactionLastResult(result)];
     },
     getFeeMargin() {
       return feeMargin;
@@ -102,25 +95,23 @@ export async function nearHelperFactory({
       return near;
     },
     async unfreezeWrappedNft(sender, to, id, txFees, nonce) {
-      const minter = await getMinter(sender);
-      const resp = minter.withdraw_nft(
-        // token_id: TokenId, chain_nonce: u8, to: String, amt: u128
-        {
-          args: {
-            token_id: id.native.tokenId,
-            chain_nonce: parseInt(nonce),
-            to,
-            amt: parseInt(txFees.toString()),
-          },
-          gas: DEFAULT_FUNCTION_CALL_GAS,
-          amount: new BN(txFees.toString()),
-        }
-      );
-      return resp;
+      const result = await sender.functionCall({
+        contractId: bridge,
+        args: {
+          token_id: id.native.tokenId,
+          chain_nonce: parseInt(nonce),
+          to,
+          amt: parseInt(txFees.toString()),
+        },
+        methodName: "withdraw_nft",
+        attachedDeposit: new BN(txFees.toString()),
+        gas: DEFAULT_FUNCTION_CALL_GAS,
+      });
+      return [result, getTransactionLastResult(result)];
     },
     async validateAddress(adr) {
       try {
-        new Account(near.connection, adr).accountId;
+        await new Account(near.connection, adr).getAccountBalance();
         return true;
       } catch (e) {
         return false;
