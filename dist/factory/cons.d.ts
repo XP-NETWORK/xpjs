aee57d86eb0453689ae1a6628e9e5fc9d9debdf2
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2022-09-18 15:47:32 +0000

    Build: (e854de0) add headers to sc-verify, remove EVM to EVM rejection

--- dist/factory/cons.d.ts
+++ dist/factory/cons.d.ts
@@ -1,5 +1,9 @@
 import { BatchExchangeRateRepo, ExchangeRateRepo } from "crypto-exchange-rate";
 import { NftInfo, FullChain } from "..";
+export declare const _headers: {
+    "Content-Type": string;
+    Accept: string;
+};
 export declare function exchangeRateRepo(baseUrl: string): ExchangeRateRepo & BatchExchangeRateRepo;
 export declare function getDefaultContract<SignerT, RawNftF, Resp, RawNftT>(nft: NftInfo<RawNftF>, fromChain: FullChain<SignerT, RawNftT, Resp>, toChain: FullChain<SignerT, RawNftT, Resp>): string | undefined;
 //# sourceMappingURL=cons.d.ts.map
