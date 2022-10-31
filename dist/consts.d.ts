f6cc254fda8f8cf3faf1f721ebeb8d405e33cf9a
imsk17 <imsumitkumar17@gmail.com>
2022-10-31 06:51:50 +0000

    Build: (fe21379) testnet: ethereum: new contracts!

--- dist/consts.d.ts
+++ dist/consts.d.ts
@@ -59,7 +59,7 @@ export declare enum MainNetRpcUri {
     GODWOKEN = "https://v1.mainnet.godwoken.io/rpc",
     GATECHAIN = "https://evm.gatenode.cc",
     VECHAIN = "https://sync-mainnet.veblocks.net",
-    SECRET = "https://grpc.mainnet.secretsaturn.net",
+    SECRET = "https://secret-4.api.trivium.network:9091",
     SKALE = "https://mainnet.skalenodes.com/v1/honorable-steel-rasalhague",
     NEAR = "https://rpc.mainnet.near.org",
     MOONBEAM = "https://rpc.api.moonbeam.network",
