91ffdac6b55a6db786dffbafaf370ed6e42c5a13
imsk17 <imsumitkumar17@gmail.com>
2022-11-10 11:47:04 +0000

    Build: (e1cf32c) aptos: handler: add support for minting tokens

--- dist/helpers/aptos/index.d.ts
+++ dist/helpers/aptos/index.d.ts
@@ -1,5 +1,5 @@
-import { ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "../chain";
-import { AptosAccount } from "aptos";
+import { ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, GetProvider, MintNft, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "../chain";
+import { AptosAccount, AptosClient } from "aptos";
 import { EvNotifier } from "../../notifier";
 export declare type AptosNFT = {
     collection_creator: string;
@@ -7,9 +7,23 @@ export declare type AptosNFT = {
     token_name: string;
     property_version: number;
 };
+/**
+ * @param collection name of the collection u already own. if u dont own any token, then set this as undefined
+ * @param name name of the NFT
+ * @param description description of the NFT
+ * @param uri The URI which the NFT points to
+ * @param createCollection set this as true if u set collection as undefined. it will create a new collection.
+ */
+export declare type AptosMintArgs = {
+    collection: string | undefined;
+    name: string;
+    description: string;
+    uri: string;
+    createCollection: boolean;
+};
 export declare type AptosHelper = ChainNonceGet & TransferNftForeign<AptosAccount, AptosNFT, string> & UnfreezeForeignNft<AptosAccount, AptosNFT, string> & EstimateTxFees<AptosNFT> & ValidateAddress & {
     XpNft: string;
-} & GetFeeMargins;
+} & GetFeeMargins & MintNft<AptosAccount, AptosMintArgs, string> & GetProvider<AptosClient>;
 export declare type AptosParams = {
     feeMargin: FeeMargins;
     rpcUrl: string;
