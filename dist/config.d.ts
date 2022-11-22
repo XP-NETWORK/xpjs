f518aade534b8428a41e123eac11779bb7590d53
imsk17 <imsumitkumar17@gmail.com>
2022-11-22 13:42:27 +0000

    Build: (db07bc4) ChainFactories: Provide Convenience Functions for Different Networks

--- dist/config.d.ts
+++ dist/config.d.ts
@@ -1,7 +1,12 @@
-import { AppConfig } from ".";
+import { AppConfig, ChainFactory } from ".";
 export declare namespace AppConfigs {
     const MainNet: () => AppConfig;
     const TestNet: () => AppConfig;
     const Staging: () => AppConfig;
+}
+export declare namespace ChainFactories {
+    const MainNet: () => Promise<ChainFactory>;
+    const TestNet: () => Promise<ChainFactory>;
+    const Staging: () => Promise<ChainFactory>;
 }
 //# sourceMappingURL=config.d.ts.map
