dafe4c34505e1f6eb9901ed35f9806933d572621
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2022-11-28 15:55:47 +0000

    Build: (8b91b56) NEAR: wallet flow - callbackURL, more args for approve

--- dist/helpers/near.d.ts
+++ dist/helpers/near.d.ts
@@ -1,7 +1,7 @@
 import { Account, Near, WalletConnection, Contract } from "near-api-js";
 import { FinalExecutionOutcome } from "near-api-js/lib/providers";
 import { EvNotifier } from "../notifier";
-import { ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, GetProvider, MintNft, NftInfo, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress, BalanceCheck } from "./chain";
+import { ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, GetProvider, MintNft, NftInfo, TransferNftForeign, UnfreezeForeignNft, ValidateAddress, BalanceCheck } from "./chain";
 declare type NearTxResult = [FinalExecutionOutcome, any];
 export declare type NearParams = {
     readonly networkId: string;
@@ -42,7 +42,11 @@ interface BrowserMethods {
     getContract(signer: Account, _contract: string): Promise<Contract>;
     getUserMinter(keypair: string, address: string): Promise<Near>;
 }
-export declare type NearHelper = ChainNonceGet & BalanceCheck & TransferNftForeign<Account, NearNFT, NearTxResult> & UnfreezeForeignNft<Account, NearNFT, NearTxResult> & MintNft<Account, NearMintArgs, NearTxResult> & EstimateTxFees<NearNFT> & Pick<PreTransfer<Account, NearNFT, string>, "preTransfer"> & ValidateAddress & {
+interface PreTransferNear {
+    preTransfer(sender: Account, nft: NftInfo<NearNFT>, fee: string, to: number, receiver: string): Promise<string | undefined>;
+    preUnfreeze(sender: Account, nft: NftInfo<NearNFT>, fee: string, to: number, receiver: string): Promise<string | undefined>;
+}
+export declare type NearHelper = ChainNonceGet & BalanceCheck & TransferNftForeign<Account, NearNFT, NearTxResult> & UnfreezeForeignNft<Account, NearNFT, NearTxResult> & MintNft<Account, NearMintArgs, NearTxResult> & EstimateTxFees<NearNFT> & Pick<PreTransferNear, "preTransfer"> & ValidateAddress & {
     XpNft: string;
     nftList(owner: Account, contract: string): Promise<NftInfo<NearNFT>[]>;
 } & GetFeeMargins & GetProvider<Near> & BrowserMethods;
