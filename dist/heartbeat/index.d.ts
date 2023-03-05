5cfc1bc56f6723c7afa279ee62848ff78e0a4ebd
Alex Moshinsky <alexmoshinsky@Alexs-MacBook-Pro-40343.local>
2023-03-05 12:55:13 +0200

    remove dist folder

--- dist/heartbeat/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { StatusResp } from "./resp";
-export type BridgeHeartbeat = {
-  status(): Promise<StatusResp>;
-};
-export declare function bridgeHeartbeat(baseURL: string): BridgeHeartbeat;
-//# sourceMappingURL=index.d.ts.map
