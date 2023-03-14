9f26feb7acb5dfef376484c627649377de78284b
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-03-14 10:09:31 +0000

    Build: (6a10cf3) NEAR: implement signatureSvc

--- dist/estimator/index.d.ts
+++ dist/estimator/index.d.ts
@@ -1,6 +1,11 @@
 import { ChainNonce } from "../type-utils";
 export interface SignatureService {
-    getSignatureNear(from: ChainNonce, toChain: ChainNonce, nft: string, tokenContract: string, tokenId: string, to: string): Promise<string>;
+    getSignatureNear(from: ChainNonce, toChain: ChainNonce, nft: string, tokenContract: string, tokenId: string, to: string): Promise<SignatureServiceResponse>;
+}
+interface SignatureServiceResponse {
+    signature: string;
+    fee: string;
 }
 export declare function signatureService(url: string): SignatureService;
+export {};
 //# sourceMappingURL=index.d.ts.map
--- dist/helpers/chain.d.ts
+++ dist/helpers/chain.d.ts
@@ -29,7 +29,7 @@ export interface PreTransfer<Signer, Nft, Ret, ExtraArgs> {
  * @returns Transaction and the Identifier of this action to track the status
  */
 export interface TransferNftForeign<Signer, RawNft, Resp> {
-    transferNftToForeign(sender: Signer, chain_nonce: number, to: string, id: NftInfo<RawNft>, txFees: BigNumber, mintWith: string, gasLimit?: ethers.BigNumberish | undefined, gasPrice?: ethers.BigNumberish | undefined): Promise<Resp>;
+    transferNftToForeign(sender: Signer, chain_nonce: number, to: string, id: NftInfo<RawNft>, txFees: BigNumber, mintWith: string, gasLimit?: ethers.BigNumberish | undefined, gasPrice?: ethers.BigNumberish | undefined): Promise<Resp | undefined>;
 }
 /**
  * Unfreeze native NFT existing on a foreign chain(Send back NFT)
