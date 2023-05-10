a30ff73679de2ad5c38a3544921e99218cc91752
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-05-10 13:41:39 +0000

    Build: (c99352b) Reject deploymentFee in unsupported chains, refactor - create scVerify Repo

--- dist/factory/utils.d.ts
+++ dist/factory/utils.d.ts
@@ -3,6 +3,7 @@ export declare const _headers: {
     "Content-Type": string;
     Accept: string;
 };
+export declare const oldXpWraps: Set<string>;
 export declare function checkBlockedContracts(to: any, contract: string): void;
 export declare function getDefaultContract<SignerT, RawNftF, Resp, RawNftT>(nft: NftInfo<RawNftF>, fromChain: FullChain<SignerT, RawNftT, Resp>, toChain: FullChain<SignerT, RawNftT, Resp>): string | undefined;
 export declare function prepareTokenId(nft: NftInfo<any>, from: number): any;
