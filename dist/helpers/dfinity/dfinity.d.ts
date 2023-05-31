a77bb57842f3c873b0ec1311198ec5ec0832f5d8
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-05-31 15:35:50 +0000

    Build: (a39289a) DFINITY: checkWhitelist, change contracts

--- dist/helpers/dfinity/dfinity.d.ts
+++ dist/helpers/dfinity/dfinity.d.ts
@@ -3,7 +3,7 @@ import { AccountIdentifier } from "@dfinity/nns";
 import { Principal } from "@dfinity/principal";
 import { SignatureService } from "../../services/estimator";
 import { EvNotifier } from "../../services/notifier";
-import { BalanceCheck, ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, MintNft, NftInfo, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress } from "../chain";
+import { BalanceCheck, ChainNonceGet, EstimateTxFees, FeeMargins, GetFeeMargins, MintNft, NftInfo, PreTransfer, TransferNftForeign, UnfreezeForeignNft, ValidateAddress, WhitelistCheck, ParamsGetter } from "../chain";
 export type DfinitySigner = Identity;
 export type DfinityNft = {
     canisterId: string;
@@ -28,7 +28,7 @@ export type DfinityHelper = ChainNonceGet & TransferNftForeign<DfinitySigner, Df
     nftList(owner: string, contract: string): Promise<NftInfo<DfinityNft>[]>;
 } & {
     getAccountIdentifier(principal: string): string;
-};
+} & WhitelistCheck<DfinityNft> & ParamsGetter<DfinityParams>;
 export type DfinityParams = {
     agent: HttpAgent;
     bridgeContract: Principal;
@@ -42,23 +42,14 @@ export declare function dfinityHelper(args: DfinityParams): Promise<DfinityHelpe
 /***
  *
  *
-{height: 6184161}
-dfinity.ts:246 6184161n 'txFeeBlock'
-
- Uncaught (in promise) Error: Call was rejected:
-  Request ID: 2b46b5f1b89494270d4866c280c75404b63736f381fbeb66aeb0bab483228c54
+Error: Call was rejected:
+  Request ID: 0ab1503fb5d82bfa487bbbf0f8ced49c2e886a30e7952bfc7612518657fdb84e
   Reject code: 5
   Reject text: Canister e3io4-qaaaa-aaaak-qasua-cai trapped explicitly: Panicked at 'called `Result::unwrap()` on an `Err` value: InvalidFee', src/minter/src/lib.rs:490:10
 
 call_on_cleanup also failed:
 
-Canister e3io4-qaaaa-aaaak-qasua-cai trapped explicitly: Panicked at 'called `Result::unwrap()` on an `Err` value: FailedToQueryFee("Failed to Query for fee. Code: NoError. Reason: cleanup")', src/minter/src/lib.rs:490:10
-
-    at pollForResponse (index.ts:68:1)
-    at async caller (actor.ts:372:1)
-    at async Object.transferNftToForeign (dfinity.ts:248:1)
-    at async onClickHandler (IcpWallet.jsx:126:1)
-pollForResponse @ index.ts:68
+Canister e3io4-qaaaa-aaaak-qasua-cai trapped explicitly: Panicked at 'called `Result::unwrap()` on an `Err` value: FailedToQueryFee("Failed to Query for fee. Code: NoError. Reason: cleanup")'
 
  */
 //# sourceMappingURL=dfinity.d.ts.map
