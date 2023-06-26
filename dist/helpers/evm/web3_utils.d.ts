5e10ae40814804d58e1114461e5ccb9a16a21fde
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-06-26 13:32:32 +0000

    Build: (577511d) HEDERA: retry 3 time to check approved

--- dist/helpers/evm/web3_utils.d.ts
+++ dist/helpers/evm/web3_utils.d.ts
@@ -1,4 +1,5 @@
 import { PopulatedTransaction } from "ethers";
 export declare const txnUnderpricedPolyWorkaround: (utx: PopulatedTransaction) => Promise<void>;
 export declare const getWrapped: (uri: string) => Promise<any>;
+export declare const tryTimes: (times: number, condition?: string) => (cb: (...args: any) => Promise<any>, ...args: any) => Promise<any>;
 //# sourceMappingURL=web3_utils.d.ts.map
