577511d1ac8cfca6aa11a245fe0a49a381b2d8c0
AlexeyAdoniev <Alexeya@xp.network>
2023-06-26 16:30:58 +0300

    HEDERA: retry 3 time to check approved

--- src/helpers/evm/web3_utils.ts
+++ src/helpers/evm/web3_utils.ts
@@ -28,3 +28,22 @@ export const getWrapped = async (uri: string) => {
   return (await axios.get(uri).catch(() => ({ data: undefined }))).data
     ?.wrapped;
 };
+
+export const tryTimes =
+  (times: number, condition: string = "") =>
+  async (cb: (...args: any) => Promise<any>, ...args: any) => {
+    for (let i = 0; i < times; i++) {
+      try {
+        const gasLimit = args.at(-1).gasLimit;
+        return await cb(...args.slice(0, -1), {
+          gasLimit: gasLimit * (i + 1),
+        });
+      } catch (error: any) {
+        console.log(`Attempt ${i + 1} failed: retry`);
+        if (condition && !error.message.includes(condition)) {
+          throw error;
+        }
+      }
+    }
+    throw new Error("PRC is not responding, please try later");
+  };
