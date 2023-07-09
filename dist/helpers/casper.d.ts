426758b82d14a8d099bc8a6e14a868403094b850
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-07-09 15:02:57 +0000

    Build: (c3ddac1) Casper: add toAccountHash method

--- dist/helpers/casper.d.ts
+++ dist/helpers/casper.d.ts
@@ -24,13 +24,16 @@ export interface CasperMintNft {
     collectionName: string;
     uri: string;
 }
+interface CasperBrowserAdapt {
+    setProxy(proxy: string): void;
+    toAccountHash(account: string): string;
+}
 export type CasperHelper = ChainNonceGet & BalanceCheck & Pick<PreTransfer<CasperLabsHelper, CasperNFT, string, undefined>, "preTransfer"> & ValidateAddress & GetFeeMargins & GetProvider<CasperClient> & {
     isApprovedForMinter(sender: CasperLabsHelper, nft: NftInfo<CasperNFT>): Promise<boolean>;
 } & TransferNftForeign<CasperLabsHelper, CasperNFT, string> & UnfreezeForeignNft<CasperLabsHelper, CasperNFT, string> & EstimateTxFees<CasperNFT> & {
     XpNft: string;
-} & GetExtraFees & MintNft<CasperLabsHelper, CasperMintNft, string> & {
-    setProxy(proxy: string): void;
-};
+} & GetExtraFees & MintNft<CasperLabsHelper, CasperMintNft, string> & CasperBrowserAdapt;
 export declare function casperHelper({ rpc, network, bridge, feeMargin, xpnft, umt, sig, notifier, }: CasperParams): Promise<CasperHelper>;
 export declare function CasperHelperFromKeys(keys: AsymmetricKey): CasperLabsHelper;
+export {};
 //# sourceMappingURL=casper.d.ts.map
