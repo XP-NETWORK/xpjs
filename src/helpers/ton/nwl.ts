2d3bcdde935a7a27cfad1e4a4907e73a89d8a04a
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-11-27 17:01:30 +0200

    Update nwl.ts

--- src/helpers/ton/nwl.ts
+++ src/helpers/ton/nwl.ts
@@ -1,1 +1,5 @@
-const nwls = ["test"];
+const nwls = [    "EQBPa7td5VDwGltzeSzsp32MkzqzlqHjAOsKtaX0mkMJhq_B",
+    "EQCFttZHA0tsLteL-w8ymLH3Wa7YeB74CDyVBUB1UtTTKAwG",
+    "EQBWRp5L4eIT3V6EvZZZxmMgylTQd169b40rnEKDrIzpfUdw",
+    "EQCwsW0Vkg-ayvgsI7Km04WBg7c-14e0GpxUIMkxg6cD-hFd",
+    "EQAGV80rmBEN13-JwfdltDwJ0JaAY-EpUQiWVDoI3QCjJybG"];
