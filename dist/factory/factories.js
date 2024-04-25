0c3be89d0a281235642fcad01dee26cef5fba737
D4mph1r <102529837+D4mph1r@users.noreply.github.com>
2024-04-25 07:10:33 +0000

    Build: (b3c17a4) change solana fee to 5

--- dist/factory/factories.js
+++ dist/factory/factories.js
@@ -854,7 +854,7 @@ var ChainFactoryConfigs;
     };
     ChainFactoryConfigs.MainNet = async () => {
         const feeMargin = { min: 0.5, max: 5 };
-        const solFeeMargin = { min: 7, max: 7 };
+        const solFeeMargin = { min: 5, max: 5 };
         const notifier = (0, notifier_1.evNotifier)(middleware_uri);
         // VeChain related:
         const net = new connex_driver_1.SimpleNet(consts_1.MainNetRpcUri.VECHAIN);
